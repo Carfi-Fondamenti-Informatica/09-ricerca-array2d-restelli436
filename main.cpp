1c5a3e5bda6c92c83990f6abd9cd0e256e13dba2
restelli436 <117980583+restelli436@users.noreply.github.com>
2022-12-17 00:43:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+int main() {
+    char lista[10][20];
+    char target[20];
+    int pos = 0;
+
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++) {
+            target[j] = ' ';
+            lista[i][j] = ' ';
+        }
+    }
+
+    for(int i=0;i<10;i++){
+        cin >> lista[i];
+    }
+
+    cin >> target;
+    if(cerca(lista,pos,target)){
+        cout << pos;
+    }else{
+        cout << "non presente";
+    }
